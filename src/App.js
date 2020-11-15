f940a1e424ced7555c41bec17556c90e569fce04
Fernando de Paula <dipaula018@gmail.com>
2020-11-14 23:15:10 -0300

    feat: basic flow

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+
+import { Navbar } from './app/Navbar'
+
+import PostsList from './features/post/PostsList'
+import AddPostForm from './features/post/AddPostForm'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <>
+                <AddPostForm />
+                <PostsList />
+              </>
+            )}
+          />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/features/post/AddPostForm.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { nanoid } from '@reduxjs/toolkit'
+
+import { postAdded } from './postsSlice'
+
+export default function AddPostForm() {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
+  const dispatch = useDispatch()
+
+  const onTitleChanged = (e) => setTitle(e.target.value)
+  const onContentChanged = (e) => setContent(e.target.value)
+
+  const onSavePostClicked = (e) => {
+    e.preventDefault()
+    if(title && content){
+      dispatch(
+        postAdded({id: nanoid(), title, content})
+      )
+
+      setTitle("")
+      setContent("")
+    }
+  }
+
+
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form onSubmit={onSavePostClicked}>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+        />
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          name="postContent"
+          id="postContent"
+          value={content}
+          onChange={onContentChanged}
+        />
+        <button type="submit">Save Post</button>
+      </form>
+    </section>
+  )
+}
